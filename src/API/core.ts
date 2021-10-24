c21a704461d48dd818544a8914eccda3b210f71c
Vitaliy Pribylo <pribylo@ukr.net>
2021-10-24 20:25:27 +0300

    Cherry-pick: 9499835e: Create httpsAgent
    # Conflicts:
    #	src/API/core.ts

--- src/API/core.ts
+++ src/API/core.ts
@@ -1,13 +1,7 @@
 import * as axios from "axios";
-import https from "https";
-
-// @ts-ignore
-const httpsAgent = new https.Agent({
-    rejectUnauthorized: false
-})
 
 // @ts-ignore
 export const instance = axios.create({
-    baseURL: 'https://localhost:5001/'
-    , httpsAgent
+    baseURL: 'http://localhost:3000/',
+
 });
