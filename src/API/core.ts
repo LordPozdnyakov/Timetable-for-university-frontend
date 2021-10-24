9499835e76a6ea84bf283dc7c8b9a98d17259159
Vitaliy Pribylo <pribylo@ukr.net>
2021-10-24 20:25:27 +0300

    Create httpsAgent

--- src/API/core.ts
+++ src/API/core.ts
@@ -1,14 +1,8 @@
 import * as axios from "axios";
-import https from "https";
-
-// @ts-ignore
-const httpsAgent = new https.Agent({
-    rejectUnauthorized: false
-})
 
 
 // @ts-ignore
 export const instance = axios.create({
     baseURL: 'http://localhost:3000/',
-    httpsAgent
+
 });
