05c5441d89c9df659c814d165950a9a43f788825
anstss <anastasiya31017@gmail.com>
2021-10-26 03:18:44 +0300

    add studentsReducer to combineReducers

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -2,6 +2,7 @@ import {applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { ProfileReducer } from "./Reducers/ProfileReducer";
 import { UserReducer } from "./Reducers/userReducer";
+import {studentsReducer} from "./Reducers/studentsReducer";
 
 type redusersType = typeof reducers;
 export type AppStateType = ReturnType<redusersType>
@@ -9,7 +10,8 @@ export type AppStateType = ReturnType<redusersType>
 
 let reducers = combineReducers({
     profile: ProfileReducer,
-    user: UserReducer
+    user: UserReducer,
+    studentsReducer
 })
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
